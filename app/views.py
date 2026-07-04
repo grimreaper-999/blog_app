bf265662deb780d712524496edc8743619357e55
Ayush <smitherwin949@gmail.com>
2026-07-04 17:28:44 +0530

    feed feature

--- app/views.py
+++ app/views.py
@@ -1,9 +1,44 @@
 from django.shortcuts import render ,redirect ,get_object_or_404
+from django.http import JsonResponse
+import random
 from django.contrib.auth.decorators import login_required
 from .models import Blog
 # Create your views here.
 @login_required
-def home(request):
+def home(view):
+    request=view
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        page= int(request.GET.get('page',1))
+        per_page=10
+        shuffled_ids =request.session.get('feed_ids',[])
+        start= (page-1)*per_page
+        end=start + per_page
+        batch_ids =shuffled_ids[start:end]
+        posts = Blog.objects.filter(id__in= batch_ids)
+        posts_by_id ={post.id:post for post in posts}
+        ordered_posts = [posts_by_id[pid] for pid in batch_ids if pid in posts_by_id]
+
+        data =[{
+            'id': post.id,
+            'title':post.title,
+            'subtitle':post.subtitle,
+            'content':post.content,
+            'author': post.author,
+            'date':post.created_at,
+
+        }for post in ordered_posts]
+        return JsonResponse({'posts':data ,'has_next':end < len(shuffled_ids)})
+    else:
+        all_ids=list(Blog.objects.values_list('id',flat=True))
+        random.shuffle(all_ids)
+
+        request.session['feed_ids']=all_ids
+        initial_ids= all_ids[0:10]
+        posts=Blog.objects.filter(id__in=initial_ids)
+        posts_by_id ={post.id: post for post in posts}
+        inittial_posts =[posts_by_id[pid] for pid in initial_ids if pid in posts_by_id]
+        return render(request,'home.html' ,{'posts': inittial_posts})
+
     return render(request,'home.html')
 @login_required
 def create(request):
