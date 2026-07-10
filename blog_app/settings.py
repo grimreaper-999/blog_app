d53c5fe7682c7029c1ddab8cd5814a8a60ce7fd0
Ayush <smitherwin949@gmail.com>
2026-07-10 09:59:55 +0530

    added neon database and ready for deploy on vercel

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+# package imported by me for backent to talk with neon database
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +78,9 @@ WSGI_APPLICATION = 'blog_app.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+    default =f"sqlite:///{os.path.join(BASE_DIR,'db.sqlite3')}",
+    conn_max_age=600)
 }
 
 
