a6cb6f9dffb23e6fb171950c06973596676d27ee
Ayush <smitherwin949@gmail.com>
2026-07-10 11:00:07 +0530

    fixed static

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -125,4 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRs =[
+    os.path.join(BASE_DIR,'static')
+]
 LOGIN_URL='login'
