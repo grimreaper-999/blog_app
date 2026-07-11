44c2d73f1f7e2b9502b6dcef75ea42fd48ae11e3
Ayush <smitherwin949@gmail.com>
2026-07-11 10:44:10 +0530

    fixed the username issue raised

--- app/views.py
+++ app/views.py
@@ -7,11 +7,16 @@ from .models import Blog
 @login_required
 def home(view):
     request=view
+    if 'feed_ids' not in request.session:
+        all_ids =list(Blog.objects.values_list('id' ,flat=True))
+        random.shuffle(all_ids)
+        request.session['feed_ids']=all_ids
     is_ajax=(
         request.GET.get('ajax') =='1' or
         request.headers.get('X-Requested-With') == 'XMLHttpRequest' or
         request.META.get('HTTP_X_REQUESTED_WITH')== 'XMLHttpRequest'
     )
+
     if is_ajax:
         page= int(request.GET.get('page',1))
         per_page=10
@@ -24,21 +29,19 @@ def home(view):
         posts = Blog.objects.filter(id__in= batch_ids)
         posts_by_id ={post.id:post for post in posts}
         ordered_posts = [posts_by_id[pid] for pid in batch_ids if pid in posts_by_id]
+        data =[]
         for post in ordered_posts:
-            author_str =post.author.username if post.author else "Annonymous"
-            date_str =post.created_at.strftime('%b %d ,%Y') if post.created_at else 'Recently'
+            author_str=post.author.username if post.author else 'Annonymous'
+            date_str = post.created_at.strftime('%b %d ,%Y') if post.created_at else "Recently"
+            data.append({
+                'id':post.id,
+                'title':post.title,
+                'subtitle':post.subtitle,
+                'content':post.content,
+                'author':author_str,
+                'date':date_str,
 
-        
-
-        data =[{
-            'id': post.id,
-            'title':post.title,
-            'subtitle':post.subtitle if post.subtitle else '',
-            'content':post.content,
-            'author': author_str,
-            'date':date_str,
-
-        }for post in ordered_posts]
+            })
         return JsonResponse({'posts':data ,'has_next':end < len(shuffled_ids)})
     else:
         all_ids=list(Blog.objects.values_list('id',flat=True))
